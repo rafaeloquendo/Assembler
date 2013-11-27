6bcbfbb8a609b3c1fbba368ca00e9265c2f0172c
rafaeloquendo <rafael.a.oquendo@hofstra.edu>
2013-11-27 17:09:36 -0500

    Sorry for the comments

---
+++ readandwritetext/readandwritetext/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace readandwritetext
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnOpen2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 107);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 231);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(125, 358);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "First Pass";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(283, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Assembler";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(227, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "by Mike Moskie and Rafael Oquendo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(289, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CSC 111";
+            // 
+            // btnOpen2
+            // 
+            this.btnOpen2.Location = new System.Drawing.Point(404, 358);
+            this.btnOpen2.Name = "btnOpen2";
+            this.btnOpen2.Size = new System.Drawing.Size(83, 23);
+            this.btnOpen2.TabIndex = 5;
+            this.btnOpen2.Text = "Second Pass";
+            this.btnOpen2.UseVisualStyleBackColor = true;
+            this.btnOpen2.Click += new System.EventHandler(this.btnOpen2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(313, 107);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 231);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(686, 443);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnOpen2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "ASSEMBLER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnOpen2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ readandwritetext/readandwritetext/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace readandwritetext
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnOpen2.Hide();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            String file_name = "\\test.txt"; // provides the location for our file name in this case its test.txt
+            String textLine = ""; // creates a textLine of string text
+            ArrayList findSubroutine = new ArrayList(); // I am creating an array to insert all individual items into the array. 
+            file_name = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + file_name; // gets my specific object txt location
+            System.IO.StreamReader objReader; // System.IO.StreamReader creates an objReader which ill use later in the program.
+            objReader = new System.IO.StreamReader(file_name);
+            
+            while (objReader.Peek() != -1)
+            {
+                textLine = objReader.ReadLine() + "\r\n"; //reads this line by line
+                findSubroutine.Add(textLine); // adds each textLine to my arraylist
+            }
+            
+            string[] s1 = new string[10]; //create a string array for better manipulation of the items added to arraylist
+            //this forloop adds each item to my string array from arraylist
+            for (int i = 0; i <= 9; i++ )
+            {
+                s1[i] = findSubroutine[i].ToString(); 
+            }
+
+            
+            string subroutine = "";
+            string test = "";
+            
+            int location = 0;
+            int numberOfSubroutine = 0;
+            
+            for (int i = 0; i < s1.Length; i++)
+            {
+                test = s1[i];
+                //MessageBox.Show(test);
+                if (test.Contains(":"))
+                {
+                    subroutine = test;
+                    subroutine = Regex.Replace(subroutine, @"[:]", "");
+                    location = i+1;
+                    numberOfSubroutine++;
+                }
+            }
+            
+            //this looks for multiple subroutines
+            string[] subroutines = new string[numberOfSubroutine];
+            int[] locations = new int[numberOfSubroutine];
+            
+            if (numberOfSubroutine > 0)
+            {
+                for (int i = 0; i < s1.Length; i++)
+                {
+                    test = s1[i];
+                    if (test.Contains(":"))
+                    {
+                        subroutines[i] = test;
+                        locations[i] = i+1;
+                    }
+                   // MessageBox.Show("Pass One Completed****\nMultiple Subroutine located : " + (i+1) + " subroutine is " + subroutines[i] + "\nIts location is at line number : "+ locations[i]);
+                }
+
+            }
+            else
+            { 
+                textBox1.Text = subroutine;
+                MessageBox.Show("Pass One Completed*****\n A Subroutine was located: " + subroutine +"\nLocated at line number " + location);
+            }
+           
+            for (int i = 0; i < subroutines.Length; i++)
+            {
+                MessageBox.Show("Pass One Completed****\nMultiple Subroutine located : " + (i+1) + " subroutine is " + subroutines[i] + "\nIts location is at line number : "+ locations[i]);
+            }
+            objReader.Close();
+            btnOpen2.Show();
+            btnOpen.Hide();
+        }
+
+        //NOTE is btnOpen2 is clicked before btnOpen1 then I created it to run btnOpen1 first
+        private void btnOpen2_Click(object sender, EventArgs e)
+        {
+            String file_name = "\\test.txt"; 
+            String textLine = ""; 
+
+            file_name = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + file_name;
+            System.IO.StreamReader objReader; 
+            objReader = new System.IO.StreamReader(file_name);
+            
+            string[] s1 = new string[10];
+            for (int i = 0; i <= 9; i++)
+            { 
+                textLine = objReader.ReadLine() + "\r\n";
+                s1[i] = textLine;
+            }
+            int[] location = new int[10];
+            //creates a location for the s1 array
+            ArrayList a1 = new ArrayList(); // will be used to print out the messagebox to textbox
+            for (int i = 0; i <= 9; i++)
+            {
+                location[i] = i;
+               // MessageBox.Show("SAVED " + s1[i] + "to INDEX " + location[i]);
+            }
+            //this for loops prints out where we saved the s1 locations
+            string text = "";
+            for (int i = 0; i <= 9; i++)
+            {
+                text = text + "SAVED " + s1[i] + "to INDEX " + location[i] + "\r\n";
+                textBox2.Text = text;
+            }
+            objReader.Close();
+            MessageBox.Show("Congrats Pass Two Completed. All Index successfully saved Index Array()");
+            btnOpen2.Hide();
+        }
+    }
+}
